0645b7464088785e28cce6deb60b7a696f071519
ayumi_arai <0araiayumi@gmail.com>
2019-05-29 15:10:38 +0900

    日本語化、質問のためのコミット

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,8 @@ module ManyoTask
                        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
+    I18n.config.available_locales = :ja
+    I18n.enforce_available_locales = false
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
