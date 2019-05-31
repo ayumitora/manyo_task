b287dfe14b2ad5225156f02c0076986e95983d40
ayumi_arai <0araiayumi@gmail.com>
2019-05-31 13:41:02 +0900

    Add:indexを作成日時順で表示

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,9 +1,7 @@
 # このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
 require 'rails_helper'
 
-# このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
-  # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
   scenario "タスク一覧のテスト" do
     Task.create!(task_name: 'test_task_01', note: 'testtesttest')
     Task.create!(task_name: 'test_task_02', note: 'samplesample')
