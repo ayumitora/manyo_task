1fa34b1d75e309ed456be271b2138daeea20863b
ayumi_arai <0araiayumi@gmail.com>
2019-06-14 15:21:25 +0900

    usersテーブル追加、コントローラー一式、newのview作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   root to: 'tasks#index'
   resources :tasks
-  # get 'tasks/index'
-  # get 'tasks/new'
-  # get 'tasks/show'
-  # get 'tasks/edit'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
