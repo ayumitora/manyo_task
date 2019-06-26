3ab598aa7636223fc80a83cfe02f5491a902a385
ayumi_arai <0araiayumi@gmail.com>
2019-06-26 16:17:03 +0900

    Add:labelモデル、label_tagモデル作成、アソシエーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,8 @@ class Task < ApplicationRecord
 
   enum priority: { not_entered: 0, high: 1, medium: 2, low: 3 }
   belongs_to :user
+  has_many :label_tags, dependent: :destroy
+  has_many :which_label, through: :label_tags, source: :label
 end
 
 
