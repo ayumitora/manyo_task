1e025441773e5e43254bd285b5d8e73b7725bd8e
ayumi_arai <0araiayumi@gmail.com>
2019-07-08 17:05:13 +0900

    heroku エラーのためのコミット

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,6 @@ module ManyoTask
     I18n.default_locale = :ja
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.version = '1.0'
   end
 end
